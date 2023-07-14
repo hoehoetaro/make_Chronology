d75e1f104859c537d99426caa4ff6cdad9b0f8aa
hoehoetaro <hoehoe@kcf.biglobe.ne.jp>
2023-07-15 07:26:16 +0900

    initial file

---
+++ 04_make_Chronology.py
@@ -0,0 +1,111 @@
+import csv
+import os
+from html import escape
+
+# Change the working directory to the script directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+def csv_to_html_table(csv_path):
+    table_html = '<h5>{}</h5>\n'.format(escape(os.path.splitext(os.path.basename(csv_path))[0]))
+    table_html += '<table>\n'
+    with open(csv_path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        headers = next(reader)  # get the first line
+        table_html += '<thead>\n<tr>\n'  # start the thead section
+        for header in headers:  # for each cell in the first row
+            table_html += f'<th>{escape(header)}</th>\n'  # create a th tag
+        table_html += '</tr>\n</thead>\n'  # end the thead section
+        for row in reader:  # for each subsequent row
+            table_html += '<tr>\n'
+            for cell in row:
+                table_html += f'<td>{escape(cell)}</td>\n'
+            table_html += '</tr>\n'
+    table_html += '</table>\n'
+    return table_html
+
+# Initialize the HTML components
+title = None
+thumbnail_path = None
+body = ''
+
+# Initialize the procedure links section
+body += '<div class="div-procedure-links">\n'
+body += '<h2>Procedure Links</h2>\n'
+body += '<ol>\n'
+
+# CSVファイルを読み込む
+procedures = []
+procedure_content = ''
+procedure_counter = 1
+with open('HTML_data.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        command = row[0]
+        content = row[1]
+
+        if command == 'title':
+            title = content
+        elif command == 'thumbnail':
+            thumbnail_path = content
+        elif command == 'procedure':
+            if procedure_content:  # If there was a previous procedure, append it
+                procedures.append((procedure_heading, procedure_content))
+                procedure_content = ''
+            procedure_heading = f'{procedure_counter}. {content}'  # Set the new heading with number
+            body += f'<li><a href="#{escape(procedure_heading)}">{escape(procedure_heading)}</a></li>\n'
+            procedure_counter += 1
+        elif command == 'description':
+            # Replace the newline character with <br> for paragraphs
+            content = content.replace('\n', '<br>')
+            procedure_content += f'<p>{content}</p>\n'
+        elif command == 'img':
+            procedure_content += f'<div class="div-img"><img src="{escape(content)}" alt="Image"></div>\n'
+        elif command == 'video':
+            procedure_content += f'<div class="div-video"><video controls onmouseover="this.play()" onmouseout="this.pause();">\n<source src="{escape(content)}" type="video/mp4">\nYour browser does not support the video tag.\n</video></div>\n'
+        elif command == 'code':
+            procedure_content += f'<div class="div-code code-container"><button onclick="copyToClipboard(\'codeBlock\')">Copy to clipboard</button>\n<pre id="codeBlock"><code>{escape(content)}</code></pre></div>\n'
+        elif command == 'table':
+            procedure_content += f'<div class="div-table">{csv_to_html_table(content)}</div>\n'
+            
+# Add title and thumbnail to body
+if thumbnail_path and os.path.exists(thumbnail_path):
+    body = f'<div class="div-img"><img src="{escape(thumbnail_path)}" alt="Image"></div>\n' + body
+if title:
+    body = f'<h1>{title}</h1>\n' + body
+
+if procedure_content:  # Append the last procedure
+    procedures.append((procedure_heading, procedure_content))
+
+body += '</ol>\n'
+body += '</div>\n'  # Close div-procedure-links
+
+# Add each procedure to the body
+for heading, content in procedures:
+    body += f'<div class="div-procedure" id="{escape(heading)}">\n'
+    body += f'<h3>{escape(heading)}</h3>\n'
+    body += content
+    body += '</div>\n'  # Close div-procedure
+
+# HTMLを出力
+output_filename = os.path.basename(os.getcwd()) + '.html'
+with open(output_filename, 'w', encoding='utf-8') as f:
+    f.write(f"""<!DOCTYPE html>
+<html>
+<head>
+    <title>{title}</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script>
+    function copyToClipboard(elementId) {{
+        var aux = document.createElement("textarea");
+        aux.innerHTML = document.getElementById(elementId).textContent;
+        document.body.appendChild(aux);
+        aux.select();
+        document.execCommand("copy");
+        document.body.removeChild(aux);
+    }}
+    </script>
+</head>
+<body>
+{body}
+</body>
+</html>""")
